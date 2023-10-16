bc99829eb8c608ee08a0d517df8ee7dc371a22c6
João Emerson <93529205+sirizinho@users.noreply.github.com>
2023-10-16 00:49:09 -0300

    updated version
    Bug fixes, and added new features

--- sidebar/script.js
+++ sidebar/script.js
@@ -1,5 +1,5 @@
 const sidebarBtn = document.querySelector(".toggle-btn");
-const sidebar = document.querySelector("aside");
+const sidebar = document.querySelector("nav");
 
 sidebarBtn.addEventListener("click", () => {
   document.body.classList.toggle("active");
@@ -7,17 +7,6 @@ sidebarBtn.addEventListener("click", () => {
 
 const links = document.querySelectorAll(".link");
 
-// Adiciona um evento de clique a cada um dos elementos
-links.forEach(link => {
-    link.addEventListener("click", function() {
-        links.forEach(otherLink => {
-            otherLink.classList.remove("active");
-        });
-
-        this.classList.add("active");
-    });
-});
-// Função para mostrar o conteúdo correspondente à opção selecionada
 function showContent(sectionId) {
   const sections = document.querySelectorAll("section");
   sections.forEach(section => {
@@ -30,10 +19,14 @@ function showContent(sectionId) {
   }
 }
 
-// Adicionar um ouvinte de evento de clique a todos os links
 links.forEach(link => {
   link.addEventListener("click", function() {
-    const sectionId = this.id; 
+    links.forEach(otherLink => {
+      otherLink.classList.remove("active");
+    });
+
+    this.classList.add("active");
+    const sectionId = this.id;
     showContent(sectionId);
   });
 });
